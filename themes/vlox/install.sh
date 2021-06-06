4a7b6d5d6b69610000e5a3663cad5a1bb7dd1d20
RTK23-dev <77203747+RTK23-dev@users.noreply.github.com>
2021-06-06 15:37:38 +0530

    Create install.sh

---
+++ themes/vlox/install.sh
@@ -0,0 +1,16 @@
+
+php /var/www/pterodactyl/artisan down
+cd /var/www/pterodactyl/resources/scripts
+rm -rf main.css
+wget https://raw.githubusercontent.com/RTK23-dev/pterotheme/main/themes/vlox/main.css
+clear
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+apt install -y nodejs
+npm i -g yarn
+cd /var/www/pterodactyl
+yarn install
+yarn add @emotion/react
+yarn build:production
+clear
+php /var/www/pterodactyl/artisan up
+echo "vlox theme Installed"
